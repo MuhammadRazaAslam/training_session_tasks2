559d9ff951273740d314ccaed2655be4a56e76df
Raza Aslam <raza.aslam@pikessoft.com>
2016-06-09 16:44:58 +0500

    p2

--- prog_2.rb
+++ prog_2.rb
@@ -1,16 +1,17 @@
 
-
 =begin
 Modify the previous program such that only multiples of 
 three or five are considered in the sum,
  e.g. 3, 5, 6, 9, 10, 12, 15 for n=17
 =end
 class Sum
-	def take_input()
+  attr_accessor:number
+
+  def initialize
     while(1)
-    	print("Enter Any Number(>0) : ")
+    	print("Enter Any Number ( > 0 ) : ")
 			@number=gets().to_i
-			if(@number<=0)
+			if( @number <= 0 )
 				puts("!!!!Invalid Entry!!!!")
 				redo
 			end
@@ -20,7 +21,7 @@ class Sum
   def cal_sum()
 			sum=0
 			for i in 1..@number do
-   				if(i%3==0 || i%5==0)
+   				if( i % 3 == 0 || i % 5 == 0 )
    					sum+=i
    				end
   		end
@@ -28,5 +29,4 @@ class Sum
 	end
 end
 s=Sum.new
-s.take_input
 s.cal_sum
