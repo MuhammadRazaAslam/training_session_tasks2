0f6b12b49ebae4d559cd09e3313ef8ce71c60c33
Raza Aslam <raza.aslam@pikessoft.com>
2016-06-09 12:05:04 +0500

    p2

--- program_2.rb
+++ program_2.rb
@@ -1,16 +1,32 @@
-while(1)
-	print("Enter Any Number(>0) : ")
-	number=gets().to_i
-	if(number<=0)
-		puts("!!!!Invalid Entry!!!!")
-		redo
+
+
+=begin
+Modify the previous program such that only multiples of 
+three or five are considered in the sum,
+ e.g. 3, 5, 6, 9, 10, 12, 15 for n=17
+=end
+class Sum
+	def take_input()
+    while(1)
+    	print("Enter Any Number(>0) : ")
+			@number=gets().to_i
+			if(@number<=0)
+				puts("!!!!Invalid Entry!!!!")
+				redo
+			end
+	  	break
+	  end
+  end
+  def cal_sum()
+			sum=0
+			for i in 1..@number do
+   				if(i%3==0 || i%5==0)
+   					sum+=i
+   				end
+  		end
+			puts("Sum of muliples of 3 or 5 from 1 to #{@number} is #{sum}")
 	end
-	break
 end
-sum=0
-for i in 1..number do
-   	if(i%3==0 || i%5==0)
-   		sum+=i
-   	end
-end
-puts("Sum of muliples of 3 or 5 from 1 to #{number} is #{sum}")
+s=Sum.new
+s.take_input
+s.cal_sum
