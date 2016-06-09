dff34c232d4587c51eb209c7d164a5eaaf7cc039
Raza Aslam <raza.aslam@pikessoft.com>
2016-06-09 13:14:16 +0500

    p4

--- program_4.rb
+++ program_4.rb
@@ -2,18 +2,29 @@
 Write a program that prints a multiplication 
 table for numbers up to 12
 =end
-while 1
-   print("Enter any number : ")
-   number=gets().to_i
-   if(number<=0)
-      puts('!!!!!Invalid Entry!!!!!')
-      redo
-    end
-    break	
+
+class Table
+	def take_input
+    while 1
+  	 	print("Enter any number : ")
+   		@number=gets().to_i
+   		if(@number<=0)
+      	puts('!!!!!Invalid Entry!!!!!')
+      	redo
+    	end
+    	break	
+		end
+	end
+	def show
+  	puts("Output is :")
+		for i in 1..12
+			result=@number*i
+			puts("#{@number} * #{i} =  #{result}")
+		end
+	end
 end
-puts("Output is :")
-for i in 1..12
-	result=number*i
-	puts("#{number} * #{i} =  #{result}")
-end
+
+table=Table.new
+table.take_input
+table.show
 
