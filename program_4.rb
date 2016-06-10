f02f80afacb19986284f6ad9c5c26257ba57bb1d
Raza Aslam <raza.aslam@pikessoft.com>
2016-06-10 08:31:40 +0500

    p4

--- program_4.rb
+++ program_4.rb
@@ -4,11 +4,13 @@ table for numbers up to 12
 =end
 
 class Table
-	def take_input
+	attr_accessor:number
+
+	def initialize
     while 1
-  	 	print("Enter any number : ")
+  	 	print("Enter any number ( > 0 ): ")
    		@number=gets().to_i
-   		if(@number<=0)
+   		if( @number <= 0 )
       	puts('!!!!!Invalid Entry!!!!!')
       	redo
     	end
@@ -18,13 +20,11 @@ class Table
 	def show
   	puts("Output is :")
 		for i in 1..12
-			result=@number*i
+			result = @number * i
 			puts("#{@number} * #{i} =  #{result}")
 		end
 	end
 end
 
 table=Table.new
-table.take_input
 table.show
-
